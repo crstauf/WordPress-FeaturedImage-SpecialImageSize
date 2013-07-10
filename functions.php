3bafdea112e7ff931c21ea90700454f5cf01cb08
crstauf <crstauf@gmail.com>
2013-07-10 15:40:13 -0400

    add $crop argument for image resizing

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
-// 'post_type' => array(width,height)
+// 'post_type' => array(width,height,crop)
 $featimg_sizes = array(
-  'post' => array(50,20)
+	'post' => array(50,20,false)
 );
 
 add_action('wp_ajax_set-post-thumbnail','generate_featimg_size',1);
@@ -14,10 +14,14 @@ function generate_featimg_size() {
 	if ('-1' == $thumbnail_id) return;
 	if (!array_key_exists($type,$featimg_sizes)) return;
 
+	$size = $featimg_sizes[$type];
+	if (!isset($size[0]) || !isset($size[1]) || empty($size[0]) || empty($size[1])) return;
+	if (!isset($size[2])) $size[2] = false; // set $crop if left blank to false
+
 	$meta = wp_get_attachment_metadata($thumbnail_id);
 	$path = apply_filters('image_make_intermediate_size',get_attached_file($thumbnail_id));
 
-	if ($newsize = image_make_intermediate_size($path,$featimg_sizes[$type][0],$featimg_sizes[$type][1])) {
+	if ($newsize = image_make_intermediate_size($path,$size[0],$size[1],$size[2])) {
 		$backupsizes = get_post_meta($thumbnail_id,'_wp_attachment_backup_sizes',true);
 		$meta['sizes']['ptfi_'.$type] = $backupsizes['ptfi_'.$type] = $newsize; // ptfi = post type featured image
 
