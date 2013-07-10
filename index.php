fded3d1befef2526484ddb403b23d6afb7bb4aff
crstauf <crstauf@gmail.com>
2013-07-10 15:30:11 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+// 'post_type' is the array key created in $featimg_sizes of functions.php
+// also, using array(width,height) will return correct image
+
+the_post_thumbnail('ptfi_{post_type}'));
+
+wp_get_attachment_image_src($thumbnail_id,'ptfi_{post_type});
